35e1aaac672a4777d1d2ac59c96c3481dc8bf0a7
Miles <milesasp@gmail.com>
2020-01-19 12:43:41 +0800

    add error output

--- main.go
+++ main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"ProtractorServer/controller"
 	"ProtractorServer/middleware"
+	"bufio"
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
 	controller.Startup()
 
 	fmt.Println("Server is running on localhost:5000")
-	http.ListenAndServe(":5000", &middleware.AllowCors{})
+	err := http.ListenAndServe(":5000", &middleware.AllowCors{})
+	fmt.Println(err)
+	fmt.Print("Press 'Enter' to exit...")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
 }
